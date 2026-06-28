db3b1c97943d68df53e0f4458586f049442bb7b2
katafuchix <katafuchix@gmail.com>
2026-06-28 16:18:38 +0900

    google map

---
+++ ios/Runner/Environment.swift
@@ -0,0 +1,13 @@
+//
+//  Environment.swift
+//  Runner
+//
+//  Created by cano on 2026/06/28.
+//
+
+
+import Foundation
+
+struct Env {
+ static let googleMapApiKey = "AIzaSyARs_KJiqqeRaat5jN32vYkrI1krGbIxqE"
+}
